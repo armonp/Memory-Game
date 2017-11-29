bd5f44895a7291c734372776e216d968b6702ac6
Armon <armon.porter@gmail.com>
2017-11-28 23:42:22 -0500

    card comparison not working

--- Desktop/fend-project-memory-game-master/js/app.js
+++ Desktop/fend-project-memory-game-master/js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-var  icons = ['bicycle', 'leaf', 'cube', 'anchor', 'paper-plane-o', 'bolt', 'diamond','bomb','bicycle', 'leaf', 'cube', 'anchor', 'paper-plane-o', 'bolt', 'diamond', 'bomb'];
+var icons = ['bicycle', 'leaf', 'cube', 'anchor', 'paper-plane-o', 'bolt', 'diamond', 'bomb', 'bicycle', 'leaf', 'cube', 'anchor', 'paper-plane-o', 'bolt', 'diamond', 'bomb'];
 
 /*
  * Display the cards on the page
@@ -19,29 +19,41 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-    $('class').text(function(i){
+    $('class').text(function (i) {
         return array[i];
-      });
+    });
 
     return array;
 };
-$(document).ready ( function () {s
-var cardList = shuffle(icons);
-//Loop through each card and create its HTML then add it to the page
-for (var i = 0; i < cardList.length; i++){
-    ($(".deck").append($('<li class="card"><i class = "fa fa-'+cardList[i] + '"></i></li>'))
-    )}
-    /*
-     * set up the event listener for a card. If a card is clicked:
-     *  - display the card's symbol (put this functionality in another function that you call from this one) */
-    $('.card').click(function() {
-       $(this).addClass('open show');
-    });
+$(document).ready(function () {
+    var cardList = shuffle(icons);
+    var openCards = [];
+    //Loop through each card and create its HTML then add it to the page
+    for (var i = 0; i < cardList.length; i++) {
+        ($(".deck").append($('<li class="card"><i class = "fa fa-' + cardList[i] + '"></i></li>'))
+        )
+    }
+    /* set up the event listener for a card. If a card is clicked, display the card's symbol. Event listner only runs if array has less two elements or less */
+    if (openCards.length <= 2) {
+        $('.card').click(function () {
+            $(this).addClass('open show');
+            /*  add the card to a *list* of "open" cards */
+            openCards.push(this);
+        });
+    }
+    /*  if the list already has another card, check to see if the two cards match */
+    /*  if the cards do match, lock the cards in the open position */
+    if (openCards.length = 2 && openCards[0] == openCards[1]) {
+        openCards = [];
+    }
+    /*  if the cards do not match, remove the cards from the list and hide the card's symbol */
+    else if (openCards.length = 2 && openCards[0] != openCards[1]) {
+        openCards[0].removeClass('open show');
+        openCards[1].removeClass('open show');
+        openCards = [];
+    }
 });
- /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+
+ /*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
