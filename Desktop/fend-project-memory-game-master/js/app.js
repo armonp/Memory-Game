971ee0bde1726379bcde85055d49e100a88ce268
Armon <armon.porter@gmail.com>
2017-12-01 12:36:27 -0500

    matching logic working

--- Desktop/fend-project-memory-game-master/js/app.js
+++ Desktop/fend-project-memory-game-master/js/app.js
@@ -34,24 +34,31 @@ $(document).ready(function () {
         )
     }
     /* set up the event listener for a card. If a card is clicked, display the card's symbol. Event listner only runs if array has less two elements or less */
-    if (openCards.length <= 2) {
         $('.card').click(function () {
+    if (openCards.length <= 2) {
+        var card = $(this);
             $(this).addClass('open show');
             /*  add the card to a *list* of "open" cards */
-            openCards.push(this);
-        });
-    }
+            openCards.push(card);
+        };
+    
     /*  if the list already has another card, check to see if the two cards match */
     /*  if the cards do match, lock the cards in the open position */
-    if (openCards.length = 2 && openCards[0] == openCards[1]) {
+    if (openCards.length == 2 && openCards[0][0].children[0].className == openCards[1][0].children[0].className) {
         openCards = [];
     }
     /*  if the cards do not match, remove the cards from the list and hide the card's symbol */
-    else if (openCards.length = 2 && openCards[0] != openCards[1]) {
-        openCards[0].removeClass('open show');
-        openCards[1].removeClass('open show');
-        openCards = [];
+    else if (openCards.length == 2 && openCards[0][0].children[0].className != openCards[1][0].children[0].className) {
+        setTimeout(function(){
+
+            openCards[0].removeClass('open show');
+            openCards[1].removeClass('open show');
+            openCards = [];
+        }, 750)
     }
+})
+//Game end condition
+
 });
 
  /*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
