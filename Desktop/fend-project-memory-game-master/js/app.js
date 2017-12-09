bb70e2ee6776e2bad8165d8aecfdc36a8ad4c744
Armon Porter <armon.porter@gmail.com>
2017-12-09 04:16:02 -0500

    updated documentation

--- Desktop/fend-project-memory-game-master/js/app.js
+++ Desktop/fend-project-memory-game-master/js/app.js
@@ -107,6 +107,7 @@ $(".restart").click(function () {
 function startTime() {
     $(".card").one("click", function () {
         if (turn === 1) {
+            /*adopted parts from https://github.com/shannonj498/memory-matching-game/blob/master/js/app.js*/
             var sec = 0;
             function time(val) { return val > 9 ? val : "0" + val; }
             timer = setInterval(function () {
